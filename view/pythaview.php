7a0421a11d0c7d1a7d96c96232deb017b9a76571
buharilife <buharilife01@gmail.com>
2025-04-23 15:55:30 +0100

    i created a triangle with css for pithagoria

--- view/pythaview.php
+++ view/pythaview.php
@@ -8,9 +8,59 @@
     <link rel="stylesheet" href="../asetholder/css/font-awasome/css/all.css">
     <link rel="stylesheet" href="../asetholder/css/maths.css">
     <title>Pythagorial</title>
+    <style>
+        .angle {
+            /* border-top-right-radius: 1000px; */
+            clip-path: polygon(0% 100%, 100% 100%, 0 0);
+            width: 150px;
+            height: 200px;
+
+        }
+
+        .A {
+            margin-top: 50px;
+           
+        }
+
+        .B {
+            margin-top: 153px;
+        }
+
+        .C {
+            margin-top: 53px;
+            margin-left: 110px;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-3">
+
+                    </div>
+                    <div class="col-lg-9 bg-dark mt-5">
+                        <div class="angle bg-danger mt-5">
+                            <strong class="A"> A </strong>
+                            <div class="B">
+                                <strong> B </strong>
+                                <strong class="C"> C </strong>
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    </div>
 
 </body>
 
