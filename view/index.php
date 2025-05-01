6d59b325476f09d6c12ac80e09f9192ac936f82d
buharilife <buharilife01@gmail.com>
2025-05-01 13:11:01 +0100

    Added a side Bar for both index and pytha page and made it beautiful with icons

--- view/index.php
+++ view/index.php
@@ -33,102 +33,117 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
         <div class="row">
             <div class=" col-lg-12 col-sm-12 col-md-6 ">
-                <div class="col-lg-3 ">
-
-                </div>
-                <div class="col-lg-9 shadow mt-5 mx-auto">
-                    <div class="row">
-                        <h6 class=" fw-bold  text-center mb-5 "><i class="me-2 fa fa-calculator "></i> Calculate from
-                            Maths to statistic Mean</h6>
-                        <div class="col-lg-4 mb-3">
-
-                            <div class="card border-warning mb-3 bg-dark mx-2">
-                                <div class="card-header text-light  text-center fw-bold fs-3">Answer</div>
-                                <div class="card-body text-light">
-                                    <?php
+                <div class="row">
+                    <div class="col-lg-3 ">
+                        <div class="list-group list-group-flush mt-5 ">
+                            <a href="index.php" class="list-group-item list-group-item-action fw-bold ">
+                                <i class=" fas fa-warehouse text-success me-3 fa-2x"></i>Arithmetic Operators
+                            </a>
+                            <a href="pythaview.php" class="list-group-item list-group-item-action fw-bold">
+                                <i class="fab fa-accusoft  fa-2x me-3 text-success "></i>Pythagoras Theorem
+                                <sup class="fw-bold">2</sup>
+                            </a>
 
 
-                                    if (!empty($num_one) && !empty($num_two)) {
-                                        switch ($operator) {
-                                            case '+':
-                                                $add = new MathController();
-                                                echo " $num_one + $num_two <br> Your answer is "
-                                                    . $add->addition($num_one, $num_two);
+                        </div>
+
+                    </div>
+                    <div class="col-lg-9 shadow  bg-black mt-5 mx-auto">
+                        <div class="row">
+                            <h6 class=" fw-bold  text-center mb-5 "><i class="me-2 fa fa-calculator "></i> Calculate
+                                from
+                                Maths to statistic Mean</h6>
+                            <div class="col-lg-4 mb-3">
+
+                                <div class="card border-success mb-3 bg-dark mx-2">
+                                    <div class="card-header text-success  text-center fw-bold fs-3 border-bottom"> <i class="fa fa-check-circle me-2 text-success"> </i> Answer
+                                    </div>
+                                    <div class="card-body text-light">
+                                        <?php
 
 
-                                                break;
+                                        if (!empty($num_one) && !empty($num_two)) {
+                                            switch ($operator) {
+                                                case '+':
+                                                    $add = new MathController();
+                                                    echo " $num_one + $num_two <br> Your answer is "
+                                                        . $add->addition($num_one, $num_two);
 
 
-                                            case '-':
-                                                $subtract = new MathController();
-                                                echo "  $num_one- $num_two  <br> Your answer is "
-                                                    . $subtract->substraction($num_one, $num_two);
-                                                break;
+                                                    break;
 
 
-                                            case '/':
-                                                $divide = new MathController();
-                                                echo "  $num_one / $num_two <br> Your answer is "
-                                                    . $divide->division($num_one, $num_two);
-                                                break;
+                                                case '-':
+                                                    $subtract = new MathController();
+                                                    echo "  $num_one- $num_two  <br> Your answer is "
+                                                        . $subtract->substraction($num_one, $num_two);
+                                                    break;
 
-                                           
-                                            // case 'mean':
-                                            //     $mean = new StatisticController();
-                                            //     echo  "  $num_onemean $num2 <br> Your answer is " . $mean->Mean($x, $y);
-                                            //     break;
-                                            // case 'mode':
-                                            //     $mode = new statistic();
-                                            //     echo  "  $x mode $y <br> Your answer is " . $mode->mode($x, $y);
-                                            //     break;
 
-                                            default:
+                                                case '/':
+                                                    $divide = new MathController();
+                                                    echo "  $num_one / $num_two <br> Your answer is "
+                                                        . $divide->division($num_one, $num_two);
+                                                    break;
 
-                                                break;
+
+                                                // case 'mean':
+                                                //     $mean = new StatisticController();
+                                                //     echo  "  $num_onemean $num2 <br> Your answer is " . $mean->Mean($x, $y);
+                                                //     break;
+                                                // case 'mode':
+                                                //     $mode = new statistic();
+                                                //     echo  "  $x mode $y <br> Your answer is " . $mode->mode($x, $y);
+                                                //     break;
+
+                                                default:
+
+                                                    break;
+                                            }
                                         }
-                                    }
 
-                                    ?>
+                                        ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-lg-5">
-                            <div class="card border-primary  bg-dark mb-3"">
-                                <div class=" card-header text-light fw-bold text-center fs-">Solve</div>
-                            <div class="card-body text-light fw-bold">
+                            <div class="col-lg-5">
+                                <div class="card border-success  bg-dark mb-3"">
+                                <div class=" card-header text-success fw-bold text-center fs-3 border-bottom "><i class=" fa fa-calculator me-2 text-success"></i>Solve</div>
+                                <div class=" card-body text-light fw-bold">
 
-                                <form action="" method="post">
+                                    <form action="" method="post">
 
-                                    <label for="number" class="mb-2"> First Number</label>
-                                    <input type="number" name="num_one"
-                                        class="form-control border-2 border-warning mb-2">
-                                    <label class="mb-2">Operators</label>
-                                    <select name="operator" class="form-control border-warning border-2" id=""> Operator
-                                        <option value="+"> add</option>
-                                        <option value="-"> -</option>
-                                        <option value="/"> /</option>
-                                        <option value="*"> *</option>
-                                        <option value="mean"> Mean</option>
-                                    </select>
-                                    <label for="" class="mb-2"> Sec Number</label>
-                                    <input type="number" name="num_two"
-                                        class="form-control border-2 border-warning mb-2">
-                                    <button type="submit" name="submit" class="btn btn-warning">
-                                        Submit
-                                    </button>
-                                    <strong></strong>
+                                        <label for="number" class="mb-2 text-success"> First Number</label>
+                                        <input type="number" name="num_one"
+                                            class="form-control border-3 border-success mb-2">
+                                        <label class="mb-2 text-success">Operators</label>
+                                        <select name="operator" class="form-control border-success border-3 " id="">
+                                            Operator
+                                            <option value="+"> + </option>
+                                            <option value="-"> - </option>
+                                            <option value="/"> /</option>
+                                            <option value="*"> *</option>
+                                            <option value="mean"> Mean</option>
+                                        </select>
+                                        <label for="" class="mb-2 text-success"> Sec Number</label>
+                                        <input type="number" name="num_two"
+                                            class="form-control border-3 border-success mb-2">
+                                        <button type="submit" name="submit" class="btn btn-success">
+                                            Submit
+                                        </button>
+                                        <strong></strong>
 
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
             </div>
-
-
-
         </div>
-    </div>
     </div>
 </body>
 
--- view/pythaview.php
+++ view/pythaview.php
@@ -1,5 +1,5 @@
  <?php
- session_start();
+
     include "../controller/PythagoController.php";
 
 
@@ -23,10 +23,37 @@
      <link rel="stylesheet" href="../asetholder/css/maths.css">
      <title>Pythagorial</title>
      <style>
-     .moveinput {
-         margin-top: 250px;
-     }
+         .smallc {
+             width: 20%;
+             position: absolute;
+             background-color: wheat;
+             top: 130px;
+             left: 240px;
+         }
+
+         .smalla {
+             width: 20%;
+             position: absolute;
+             top: 130px;
+             left: 20px;
+             background-color: wheat;
+         }
+
+         .moveinput {
+             margin-top: 250px;
+         }
+
+         .smallb {
+             width: 20%;
+             position: absolute;
+             border-top-right-radius: 20px !important;
+             border-top-left-radius: 20px !important;
+             background-color: wheat;
+             top: 260px;
+             left: 140px;
+         }
      </style>
+
 
  </head>
 
@@ -36,34 +63,51 @@
              <div class="col-lg-12">
                  <div class="row">
                      <div class="col-lg-3">
+                         <div class="list-group list-group-flush mt-5 ">
+                             <a href="index.php" class="list-group-item list-group-item-action fw-bold ">
+                                 <i class=" fas fa-warehouse text-success me-3 fa-2x"></i>Arithmetic Operators
+                             </a>
+                             <a href="pythaview.php" class="list-group-item list-group-item-action fw-bold">
+                                 <i class="fab fa-accusoft  fa-2x me-3 text-success "></i>Pythagoras Theorem
+                                 <sup class="fw-bold">2</sup>
+                             </a>
+
+
+                         </div>
+
+                         <div class="list">
+
+                         </div>
 
                      </div>
                      <div class="col-lg-9 bg-dark mt-5">
                          <div class="row">
                              <div class="col-lg-5">
                                  <div class="card border-success border-2 bg-dark mb-3 mt-5 p-2 ">
-                                     <div class="card-header text-success fw-bold text-center border-bottom"><i
-                                             class=" fa fa-calculator me-2"></i>PYTHAGORAS </div>
+                                     <div class="card-header text-success fw-bold text-center border-bottom fw-bold">
+                                         <i class=" fa fa-calculator me-2"></i> PYTHAGORAS
+                                     </div>
                                      <div class="card-body text-primary">
-                                         <div class="smallc text-center"> c</div>
-                                         <div class="smalla text-center">a</div>
-                                         <div class="smallb text-center">b</div>
+                                         <div class="smallc text-center"> <i class="fa fa-eye"></i> c</div>
+                                         <div class="smalla text-center"> <i class="fa fa-eye "></i>a</div>
+                                         <div class="smallb text-center">
+                                             <i class="fa fa-teeth-open fa-1x"></i> b
+                                         </div>
 
 
 
-                                         <div class="angle bg-danger mt-2 mx-auto">
+                                         <div class=" angle bg-danger mt-2 mx-auto">
 
                                          </div>
                                      </div>
                                      <form action="" method="POST">
-
-
                                          <input type="number" id="" name="num_one"
                                              class="form-control moveinput mb-2  border-success border-3 "
-                                             placeholder="Enter num one">
+                                             placeholder="First Number">
                                          <select name="operator" class="form-control border-success border-3 mb-2"
                                              id="">
                                              Operator
+                                             <option value="operator"> Operator </option>
                                              <option value="C"> Ab is giving find c</option>
                                              <option value="B"> Ac is given find B</option>
                                              <option value="A"> Bc is given find A</option>
@@ -71,7 +115,7 @@
                                          </select>
                                          <input type="number" id="" name="num_two"
                                              class="form-control mb-3  border-success border-3"
-                                             placeholder="Enter num two ">
+                                             placeholder="Second Number">
 
 
                                          <button type="submit" name="submit" class="btn btn-lg w-50 btn-success  ">
@@ -88,24 +132,24 @@
                                      <div class="card-body text-light">
                                          <br>
                                          <?php
-                                      
+
                                             if (!empty($num_one) && !empty($num_two)) {
                                                 switch ($operator) {
                                                     case 'C':
                                                         $pythagoria = new  PythagoController();
-                                                        
+
 
                                                         echo "The Pythagorial theorem of <br> $num_one & $num_two 
                                                          = " . $pythagoria->pathagoriaPlus($num_one, $num_two);
 
 
                                                         break;
-                                                            case 'B':
+                                                    case 'B':
                                                         $pythagoria = new  PythagoController();
 
                                                         echo "The Pythagorial theorem of <br> $num_one & $num_two 
                                                          = " . $pythagoria->pathagoriaMinus($num_one, $num_two);
-                                                         break;
+                                                        break;
                                                     case 'A':
                                                         $pythagoria = new  PythagoController();
 
@@ -117,11 +161,11 @@
 
 
                                                     default:
-                                                 
+
                                                         break;
                                                 }
                                             }
-                                          
+
 
                                             ?>
                                      </div>
